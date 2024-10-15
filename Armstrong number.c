4350b693de51f9926e522231f4e4a9dade737509
ManukondaRamyaSri787 <24b01a0591@svecw.edu.in>
2024-10-15 22:39:25 +0530

    Update Armstrong number.c

--- Armstrong number.c
+++ Armstrong number.c
@@ -1,23 +1,32 @@
 
 /*To find the given number is Armstrong number or not*/
-#include <stdio.h>
-int main() {
-    int num, originalNum, remainder, result = 0;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    originalNum = num;
 
-    while (originalNum != 0) {
-       
-        remainder = originalNum % 10;
-        result += remainder * remainder * remainder;
-        originalNum /= 10;
-    }
+ #include <stdio.h>   
+#include <math.h>     
+int main()
+{
+int n1, onum, r, result = 0, n = 0 ;
 
-    if (result == num)
-        printf("%d is an Armstrong number.", num);
-    else
-        printf("%d is not an Armstrong number.", num);
-
+printf(" Input an integer : ");
+scanf("%d", &n1);
     
-}
+onum = n1;
+    
+while (onum != 0){
+onum /= 10;
+        ++n;}
+    
+onum = n1;
+    
+while (onum != 0)
+    {
+        r = onum % 10;
+result += pow(r, n);
+onum /= 10;
+    }
+    
+if(result == n1)
+printf(" %d is an Armstrong number.\n\n", n1);
+else
+printf(" %d is not an Armstrong number.\n\n", n1);
+},   
